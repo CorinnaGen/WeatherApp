03a65dd9d9ca6273bcb6bb90971c323a54b0a67e
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-02-28 17:59:26 +0100

    js hourlyforecast current loc correct

--- src/finalproject.js
+++ src/finalproject.js
@@ -96,10 +96,7 @@ function showTemperature(response) {
 function currentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
- 
-
-   
- 
+  
 }
 
 function searchLocation(position) {
@@ -109,8 +106,9 @@ function searchLocation(position) {
   let apiKey = "94128e0a800f0999e0bbd83894a5cfd3";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
-    let  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+   let  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
+   
   
  
 }
