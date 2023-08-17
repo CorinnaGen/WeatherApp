0e3cc387e11eb021a2e7589d453ff87ec8dca809
Corinna Genchi <corinna.genchi@porsche.digital>
2023-08-17 21:59:53 +0200

    hiding apikey 2 years later bc im ashemed of past

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-let apiKey = "94128e0a800f0999e0bbd83894a5cfd3";
+const apiKey = config.ApiKey
 let units = "metric";
 
 
@@ -7,12 +7,12 @@ function showCity(event) {
   let cityForm = document.querySelector("#city-input");
   let h1 = document.querySelector("#heading");
   h1.innerHTML = `${cityForm.value}`;
-  searchCity(cityForm.value) 
+  searchCity(cityForm.value)
 }
 function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
-  apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -26,7 +26,7 @@ function showTemperature(response) {
   let currentCity = document.querySelector("#heading");
   currentCity.innerHTML = response.data.name;
   let description = document.querySelector("#description");
-  description.innerHTML= `${response.data.weather[0].description}`;
+  description.innerHTML = `${response.data.weather[0].description}`;
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `${response.data.main.humidity}%`;
   let wind = document.querySelector("#wind");
@@ -35,45 +35,48 @@ function showTemperature(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   celsiusTemp = Math.round(response.data.main.temp);
   getForecast(response.data.coord);
-  }
+}
 
 // Forecast per hours and per days
 
-function displayForecast(response){
+function displayForecast(response) {
   //console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = null;
   let forecast = response.data.list;
-  forecast.forEach((val, i)=>{
-  if(i <4){
-  forecastElement.innerHTML += `<div class="col-3">
+  forecast.forEach((val, i) => {
+    if (i < 4) {
+      forecastElement.innerHTML += `<div class="col-3">
           ${formatHours(val.dt * 1000)} h
           <br />
           <img src="http://openweathermap.org/img/wn/${val.weather[0].icon}@2x.png"/><br>
           <strong>${Math.round(val.main.temp_max)}º</strong>||${Math.round(val.main.temp_min)}º
         </div>`
-  }});
+    }
+  });
 }
-  function getForecast(coordinates){
+function getForecast(coordinates) {
   apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeeklyForecast);
 }
 
-function displayWeeklyForecast(response){
+function displayWeeklyForecast(response) {
   //console.log(response.data.daily);
-let weeklyForecastElement = document.querySelector("#weekly-forecast");
-   weeklyForecastElement.innerHTML= null;
-let weeklyForecast = response.data.daily;
+  let weeklyForecastElement = document.querySelector("#weekly-forecast");
+  weeklyForecastElement.innerHTML = null;
+  let weeklyForecast = response.data.daily;
 
- weeklyForecast.forEach((val, i )=>{
-   if (i < 4){
-  weeklyForecastElement.innerHTML += `<div class="col-3">
+  weeklyForecast.forEach((val, i) => {
+    if (i < 4) {
+      weeklyForecastElement.innerHTML += `<div class="col-3">
 <span><h5>${formatDay(val.dt)}</h5></span>
 <br/>
 <img src="http://openweathermap.org/img/wn/${val.weather[0].icon}@2x.png" />
 <div>
-<strong>${Math.round(val.temp.max)}</strong>º|| º${Math.round(val.temp.min)}</div></div>`}});
- }
+<strong>${Math.round(val.temp.max)}</strong>º|| º${Math.round(val.temp.min)}</div></div>`
+    }
+  });
+}
 
 
 //shows forecast of the current location
@@ -81,16 +84,16 @@ let weeklyForecast = response.data.daily;
 function currentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
-  
+
 }
 function searchLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
-   apiUrl= `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
-;
+  ;
 }
 
 let currentButton = document.querySelector("#current-button");
